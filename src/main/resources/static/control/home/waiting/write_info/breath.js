438864893f241c2b30f3282856d25dcbf3199ec1
9ml <508085251@qq.com>
2019-07-22 09:46:40 +0800

    update

--- src/main/resources/static/control/home/waiting/write_info/breath.js
+++ src/main/resources/static/control/home/waiting/write_info/breath.js
@@ -7,8 +7,25 @@ window.onload = function(){
         $("#cqz1Swc").val((parseInt($("#cqzhp11").val())-parseInt($("#cqzcp12").val()))/parseInt($("#cqzcp12").val()));
         if(parseInt($("#cqz1Swc"))>-0.15 && parseInt($("#cqz1Swc"))<0.15){
             $("#wc1").text("符合");
+            $("#wc1").removeClass("no").addClass("is");
         }else{
             $("#wc1").text("不符合");
+            $("#wc1").removeClass("is").addClass("no");
+        }
+    });
+
+    $("#cqzh23").blur(function(){
+        $("#cqzhp21").val((parseInt($("#cqzh21").val())+parseInt($("#cqzh22").val())+parseInt($("#cqzh23").val()))/3);
+    });
+    $("#cqzc23").blur(function(){
+        $("#cqzcp22").val((parseInt($("#cqzc21").val())+parseInt($("#cqzc22").val())+parseInt($("#cqzc23").val()))/3);
+        $("#cqz2Swc").val((parseInt($("#cqzhp21").val())-parseInt($("#cqzcp22").val()))/parseInt($("#cqzcp22").val()));
+        if(parseInt($("#cqz2Swc"))>-0.15 && parseInt($("#cqz2Swc"))<0.15){
+            $("#wc2").text("符合");
+            $("#wc2").removeClass("no").addClass("is");
+        }else{
+            $("#wc2").text("不符合");
+            $("#wc2").removeClass("is").addClass("no");
         }
     });
 }
