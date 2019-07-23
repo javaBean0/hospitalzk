54f47fbd28d3770e97fb1c4e2d86a462ebfb5f69
9ml <508085251@qq.com>
2019-07-23 14:49:38 +0800

    update

---
+++ src/main/resources/static/control/home/waiting/write_info/defibrillator.js
@@ -0,0 +1,161 @@
+window.onload = function(){
+    // 最小能量值
+    $("#sfnlValue1").blur(function(){
+        $("#sfnlWc1").val(parseInt($("#zxnl").val()) - parseInt($("#sfnlValue1").val()));
+        if($("#sfnlWc1").val > -0.15 && $("#sfnlWc1").val() < 0.15){
+            $("#wc1").text("符合");
+            $("#wc1").removeClass("no").addClass("is");
+        }else{
+            $("#wc1").text("不符合"); 
+            $("#wc1").removeClass("is").addClass("no");
+        }
+    });
+    // 30
+    $("#sfnlValue2").blur(function(){
+        $("#sfnlWc2").val(30 - parseInt($("#sfnlValue2").val()));
+        if($("#sfnlWc2").val > -0.15 && $("#sfnlWc2").val() < 0.15){
+            $("#wc2").text("符合");
+            $("#wc2").removeClass("no").addClass("is");
+        }else{
+            $("#wc2").text("不符合"); 
+            $("#wc2").removeClass("is").addClass("no");
+        }
+    });
+    // 50
+    $("#sfnlValue3").blur(function(){
+        $("#sfnlWc3").val(50 - parseInt($("#sfnlValue3").val()));
+        if($("#sfnlWc3").val > -0.15 && $("#sfnlWc3").val() < 0.15){
+            $("#wc3").text("符合");
+            $("#wc3").removeClass("no").addClass("is");
+        }else{
+            $("#wc3").text("不符合"); 
+            $("#wc3").removeClass("is").addClass("no");
+        }
+    });
+    // 最大能量值
+    $("#sfnlValue6").blur(function(){
+        $("#sfnlWc6").val(parseInt($("#zdnl").val()) - parseInt($("#sfnlValue6").val()));
+        if($("#sfnlWc6").val > -0.15 && $("#sfnlWc6").val() < 0.15){
+            $("#wc4").text("符合");
+            $("#wc4").removeClass("no").addClass("is");
+        }else{
+            $("#wc4").text("不符合"); 
+            $("#wc4").removeClass("is").addClass("no");
+        }
+    });
+    // 100
+    $("#sfnlValue4").blur(function(){
+        $("#sfnlWc4").val(100 - parseInt($("#sfnlValue4").val()));
+        if($("#sfnlWc4").val > -0.15 && $("#sfnlWc4").val() < 0.15){
+            $("#wc5").text("符合");
+            $("#wc5").removeClass("no").addClass("is");
+        }else{
+            $("#wc5").text("不符合"); 
+            $("#wc5").removeClass("is").addClass("no");
+        }
+    });
+    // 200
+    $("#sfnlValue5").blur(function(){
+        $("#sfnlWc5").val(200 - parseInt($("#sfnlValue5").val()));
+        if($("#sfnlWc5").val > -0.15 && $("#sfnlWc5").val() < 0.15){
+            $("#wc6").text("符合");
+            $("#wc6").removeClass("no").addClass("is");
+        }else{
+            $("#wc6").text("不符合"); 
+            $("#wc6").removeClass("is").addClass("no");
+        }
+    });
+    // 20 前 中 后
+    $("#sfnlValue73").blur(function(){
+        $("#sfnlWc7").val(20 - (parseInt($("#sfnlValue71").val())+parseInt($("#sfnlValue72").val())+parseInt($("#sfnlValue73").val()))/3);
+        if($("#sfnlWc7").val > -0.15 && $("#sfnlWc7").val() < 0.15){
+            $("#wc7").text("符合");
+            $("#wc7").removeClass("no").addClass("is");
+        }else{
+            $("#wc7").text("不符合"); 
+            $("#wc7").removeClass("is").addClass("no");
+        }
+    });
+    // 200 前 中 后
+    $("#sfnlValue83").blur(function(){
+        $("#sfnlWc8").val(200 - (parseInt($("#sfnlValue81").val())+parseInt($("#sfnlValue82").val())+parseInt($("#sfnlValue83").val()))/3);
+        if($("#sfnlWc8").val > -0.15 && $("#sfnlWc8").val() < 0.15){
+            $("#wc8").text("符合");
+            $("#wc8").removeClass("no").addClass("is");
+        }else{
+            $("#wc8").text("不符合"); 
+            $("#wc8").removeClass("is").addClass("no");
+        }
+    });
+    // 充电时间
+    $("#cdsjValue").blur(function(){
+        if($("#cdsjValue").val() > 0 && $("#cdsjValue").val() <= 20){
+            $("#wc9").text("符合");
+            $("#wc9").removeClass("no").addClass("is");
+        }else{
+            $("#wc9").text("不符合"); 
+            $("#wc9").removeClass("is").addClass("no");
+        }
+    });
+    // 同步延迟时间
+    $("#tbmsSj").blur(function(){
+        if($("#tbmsSj").val() > 0  && $("#tbmsSj").val() <= 60){
+            $("#wc10").text("符合");
+            $("#wc10").removeClass("no").addClass("is");
+        }else{
+            $("#wc10").text("不符合"); 
+            $("#wc10").removeClass("is").addClass("no");
+        }
+    });
+    // 心率示值
+    // 30
+    $("#xlszValue1").blur(function(){
+        if($("#xlszValue1").val() >= 28  && $("#xlszValue1").val() <= 32){
+            $("#xl1").text("符合");
+            $("#xl1").removeClass("no").addClass("is");
+        }else{
+            $("#xl1").text("不符合");
+            $("#xl1").removeClass("is").addClass("no");
+        }
+    });
+    // 60
+    $("#xlszValue2").blur(function(){
+        if($("#xlszValue2").val() >= 57  && $("#xlszValue2").val() <= 63){
+            $("#xl2").text("符合");
+            $("#xl2").removeClass("no").addClass("is");
+        }else{
+            $("#xl2").text("不符合");
+            $("#xl2").removeClass("is").addClass("no");
+        }
+    });
+    // 100
+    $("#xlszValue3").blur(function(){
+        if($("#xlszValue3").val() >= 95  && $("#xlszValue3").val() <= 105){
+            $("#xl3").text("符合");
+            $("#xl3").removeClass("no").addClass("is");
+        }else{
+            $("#xl3").text("不符合");
+            $("#xl3").removeClass("is").addClass("no");
+        }
+    });
+    // 120
+    $("#xlszValue4").blur(function(){
+        if($("#xlszValue4").val() >= 114  && $("#xlszValue4").val() <= 126){
+            $("#xl4").text("符合");
+            $("#xl4").removeClass("no").addClass("is");
+        }else{
+            $("#xl4").text("不符合");
+            $("#xl4").removeClass("is").addClass("no");
+        }
+    });
+    // 180
+    $("#xlszValue5").blur(function(){
+        if($("#xlszValue5").val() >= 171  && $("#xlszValue5").val() <= 189){
+            $("#xl5").text("符合");
+            $("#xl5").removeClass("no").addClass("is");
+        }else{
+            $("#xl5").text("不符合");
+            $("#xl5").removeClass("is").addClass("no");
+        }
+    });
+}
