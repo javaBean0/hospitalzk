00398e955ca9abedebd7d6022f90911cf143de7c
9ml <508085251@qq.com>
2019-08-05 16:18:52 +0800

    1

--- src/main/resources/static/control/home/waiting/write_info/breath.js
+++ src/main/resources/static/control/home/waiting/write_info/breath.js
@@ -2,11 +2,11 @@ window.onload = function(){
 // 潮气量
     // 400
     $("#cqzh13").blur(function(){
-        $("#cqzhp11").val((parseInt($("#cqzh11").val())+parseInt($("#cqzh12").val())+parseInt($("#cqzh13").val()))/3);
+        $("#cqzhp11").val(((parseFloat($("#cqzh11").val())+parseFloat($("#cqzh12").val())+parseFloat($("#cqzh13").val()))/3).toFixed(1));
     });
     $("#cqzc13").blur(function(){
-        $("#cqzcp12").val((parseInt($("#cqzc11").val())+parseInt($("#cqzc12").val())+parseInt($("#cqzc13").val()))/3);
-        $("#cqz1Swc").val((parseInt($("#cqzhp11").val())-parseInt($("#cqzcp12").val()))/parseInt($("#cqzcp12").val()));
+        $("#cqzcp12").val(((parseFloat($("#cqzc11").val())+parseFloat($("#cqzc12").val())+parseFloat($("#cqzc13").val()))/3).toFixed(1));
+        $("#cqz1Swc").val(((parseFloat($("#cqzhp11").val())-parseFloat($("#cqzcp12").val()))/parseFloat($("#cqzcp12").val())).toFixed(1));
         if($("#cqz1Swc").val() >= 340 && $("#cqz1Swc").val() <= 460){
             $("#wc1").text("符合");
             $("#wc1").removeClass("no").addClass("is");
@@ -17,11 +17,11 @@ window.onload = function(){
     });
     // 600
     $("#cqzh23").blur(function(){
-        $("#cqzhp21").val((parseInt($("#cqzh21").val())+parseInt($("#cqzh22").val())+parseInt($("#cqzh23").val()))/3);
+        $("#cqzhp21").val(((parseFloat($("#cqzh21").val())+parseFloat($("#cqzh22").val())+parseFloat($("#cqzh23").val()))/3).toFixed(1));
     });
     $("#cqzc23").blur(function(){
-        $("#cqzcp22").val((parseInt($("#cqzc21").val())+parseInt($("#cqzc22").val())+parseInt($("#cqzc23").val()))/3);
-        $("#cqz2Swc").val((parseInt($("#cqzhp21").val())-parseInt($("#cqzcp22").val()))/parseInt($("#cqzcp22").val()));
+        $("#cqzcp22").val(((parseFloat($("#cqzc21").val())+parseFloat($("#cqzc22").val())+parseFloat($("#cqzc23").val()))/3).toFixed(1));
+        $("#cqz2Swc").val(((parseFloat($("#cqzhp21").val())-parseFloat($("#cqzcp22").val()))/parseFloat($("#cqzcp22").val())).toFixed(1));
         if($("#cqz2Swc").val() >= 510 && $("#cqz2Swc").val() <= 690){
             $("#wc2").text("符合");
             $("#wc2").removeClass("no").addClass("is");
@@ -32,11 +32,11 @@ window.onload = function(){
     });
     // 800 
     $("#cqzh33").blur(function(){
-        $("#cqzhp31").val((parseInt($("#cqzh31").val())+parseInt($("#cqzh32").val())+parseInt($("#cqzh33").val()))/3);
+        $("#cqzhp31").val(((parseFloat($("#cqzh31").val())+parseFloat($("#cqzh32").val())+parseFloat($("#cqzh33").val()))/3).toFixed(1));
     });
     $("#cqzc33").blur(function(){
-        $("#cqzcp32").val((parseInt($("#cqzc31").val())+parseInt($("#cqzc32").val())+parseInt($("#cqzc33").val()))/3);
-        $("#cqz3Swc").val((parseInt($("#cqzhp31").val())-parseInt($("#cqzcp32").val()))/parseInt($("#cqzcp32").val()));
+        $("#cqzcp32").val(((parseFloat($("#cqzc31").val())+parseFloat($("#cqzc32").val())+parseFloat($("#cqzc33").val()))/3).toFixed(1));
+        $("#cqz3Swc").val(((parseFloat($("#cqzhp31").val())-parseFloat($("#cqzcp32").val()))/parseFloat($("#cqzcp32").val())).toFixed(1));
         if($("#cqz3Swc").val() >= 680 && $("#cqz3Swc").val() <= 920){
             $("#wc3").text("符合");
             $("#wc3").removeClass("no").addClass("is");
@@ -48,11 +48,11 @@ window.onload = function(){
 // 强制通气频率
     // 10
     $("#qztqh13").blur(function(){
-        $("#qztqhp11").val((parseInt($("#qztqh11").val())+parseInt($("#qztqh12").val())+parseInt($("#qztqh13").val()))/3);
+        $("#qztqhp11").val(((parseFloat($("#qztqh11").val())+parseFloat($("#qztqh12").val())+parseFloat($("#qztqh13").val()))/3).toFixed(1));
     });
     $("#qztqc13").blur(function(){
-        $("#qztqcp12").val((parseInt($("#qztqc11").val())+parseInt($("#qztqc12").val())+parseInt($("#qztqc13").val()))/3);
-        $("#qztq1Swc").val((parseInt($("#qztqhp11").val())-parseInt($("#qztqcp12").val()))/parseInt($("#qztqcp12").val()));
+        $("#qztqcp12").val(((parseFloat($("#qztqc11").val())+parseFloat($("#qztqc12").val())+parseFloat($("#qztqc13").val()))/3).toFixed(1));
+        $("#qztq1Swc").val(((parseFloat($("#qztqhp11").val())-parseFloat($("#qztqcp12").val()))/parseFloat($("#qztqcp12").val())).toFixed(1));
         if($("#qztq1Swc").val() >= 9 && $("#qztq1Swc").val() <= 11){
             $("#wc4").text("符合");
             $("#wc4").removeClass("no").addClass("is");
@@ -63,11 +63,11 @@ window.onload = function(){
     });
     // 25
     $("#qztqh23").blur(function(){
-        $("#qztqhp21").val((parseInt($("#qztqh21").val())+parseInt($("#qztqh22").val())+parseInt($("#qztqh23").val()))/3);
+        $("#qztqhp21").val(((parseFloat($("#qztqh21").val())+parseFloat($("#qztqh22").val())+parseFloat($("#qztqh23").val()))/3).toFixed(1));
     });
     $("#qztqc23").blur(function(){
-        $("#qztqcp22").val((parseInt($("#qztqc21").val())+parseInt($("#qztqc22").val())+parseInt($("#qztqc23").val()))/3);
-        $("#qztq2Swc").val((parseInt($("#qztqhp21").val())-parseInt($("#qztqcp22").val()))/parseInt($("#qztqcp22").val()));
+        $("#qztqcp22").val(((parseFloat($("#qztqc21").val())+parseFloat($("#qztqc22").val())+parseFloat($("#qztqc23").val()))/3).toFixed(1));
+        $("#qztq2Swc").val(((parseFloat($("#qztqhp21").val())-parseFloat($("#qztqcp22").val()))/parseFloat($("#qztqcp22").val())).toFixed(1));
         if($("#qztq2Swc").val() >= 22.5 && $("#qztq2Swc").val() <= 27.5){
             $("#wc5").text("符合");
             $("#wc5").removeClass("no").addClass("is");
@@ -78,11 +78,11 @@ window.onload = function(){
     });
     // 40
     $("#qztqh33").blur(function(){
-        $("#qztqhp31").val((parseInt($("#qztqh31").val())+parseInt($("#qztqh32").val())+parseInt($("#qztqh33").val()))/3);
+        $("#qztqhp31").val(((parseFloat($("#qztqh31").val())+parseFloat($("#qztqh32").val())+parseFloat($("#qztqh33").val()))/3).toFixed(1));
     });
     $("#qztqc33").blur(function(){
-        $("#qztqcp32").val((parseInt($("#qztqc31").val())+parseInt($("#qztqc32").val())+parseInt($("#qztqc33").val()))/3);
-        $("#qztq3Swc").val((parseInt($("#qztqhp31").val())-parseInt($("#qztqcp32").val()))/parseInt($("#qztqcp32").val()));
+        $("#qztqcp32").val(((parseFloat($("#qztqc31").val())+parseFloat($("#qztqc32").val())+parseFloat($("#qztqc33").val()))/3).toFixed(1));
+        $("#qztq3Swc").val(((parseFloat($("#qztqhp31").val())-parseFloat($("#qztqcp32").val()))/parseFloat($("#qztqcp32").val())).toFixed(1));
         if($("#qztq3Swc").val() >= 36 && $("#qztq3Swc").val() <= 44){
             $("#wc6").text("符合");
             $("#wc6").removeClass("no").addClass("is");
@@ -94,11 +94,11 @@ window.onload = function(){
 // 吸入氧浓度
     // 21
     $("#xryh13").blur(function(){
-        $("#xryhp11").val((parseInt($("#xryh11").val())+parseInt($("#xryh12").val())+parseInt($("#xryh13").val()))/3);
+        $("#xryhp11").val(((parseFloat($("#xryh11").val())+parseFloat($("#xryh12").val())+parseFloat($("#xryh13").val()))/3).toFixed(1));
     });
     $("#xryc13").blur(function(){
-        $("#xrycp12").val((parseInt($("#xryc11").val())+parseInt($("#xryc12").val())+parseInt($("#xryc13").val()))/3);
-        $("#xry1Swc").val(parseInt($("#xryhp11").val())-parseInt($("#xrycp12").val()));
+        $("#xrycp12").val(((parseFloat($("#xryc11").val())+parseFloat($("#xryc12").val())+parseFloat($("#xryc13").val()))/3).toFixed(1));
+        $("#xry1Swc").val(parseFloat($("#xryhp11").val())-parseFloat($("#xrycp12").val()));
         if($("#xry1Swc").val() >= 19.95 && $("#xry1Swc").val() <= 22.05){
             $("#wc7").text("符合");
             $("#wc7").removeClass("no").addClass("is");
@@ -109,11 +109,11 @@ window.onload = function(){
     });
     // 60
     $("#xryh23").blur(function(){
-        $("#xryhp21").val((parseInt($("#xryh21").val())+parseInt($("#xryh22").val())+parseInt($("#xryh23").val()))/3);
+        $("#xryhp21").val(((parseFloat($("#xryh21").val())+parseFloat($("#xryh22").val())+parseFloat($("#xryh23").val()))/3).toFixed(1));
     });
     $("#xryc23").blur(function(){
-        $("#xrycp22").val((parseInt($("#xryc21").val())+parseInt($("#xryc22").val())+parseInt($("#xryc23").val()))/3);
-        $("#xry2Swc").val(parseInt($("#xryhp21").val())-parseInt($("#xrycp22").val()));
+        $("#xrycp22").val(((parseFloat($("#xryc21").val())+parseFloat($("#xryc22").val())+parseFloat($("#xryc23").val()))/3).toFixed(1));
+        $("#xry2Swc").val(parseFloat($("#xryhp21").val())-parseFloat($("#xrycp22").val()));
         if($("#xry2Swc").val() >= 57 && $("#xry2Swc").val() <= 63){
             $("#wc8").text("符合");
             $("#wc8").removeClass("no").addClass("is");
@@ -124,11 +124,11 @@ window.onload = function(){
     });
     // 100
     $("#xryh33").blur(function(){
-        $("#xryhp31").val((parseInt($("#xryh31").val())+parseInt($("#xryh32").val())+parseInt($("#xryh33").val()))/3);
+        $("#xryhp31").val(((parseFloat($("#xryh31").val())+parseFloat($("#xryh32").val())+parseFloat($("#xryh33").val()))/3).toFixed(1));
     });
     $("#xryc33").blur(function(){
-        $("#xrycp32").val((parseInt($("#xryc31").val())+parseInt($("#xryc32").val())+parseInt($("#xryc33").val()))/3);
-        $("#xry3Swc").val(parseInt($("#xryhp31").val())-parseInt($("#xrycp32").val()));
+        $("#xrycp32").val(((parseFloat($("#xryc31").val())+parseFloat($("#xryc32").val())+parseFloat($("#xryc33").val()))/3).toFixed(1));
+        $("#xry3Swc").val(parseFloat($("#xryhp31").val())-parseFloat($("#xrycp32").val()));
         if($("#xry3Swc").val() >= 95 && $("#xry3Swc").val() <= 105){
             $("#wc9").text("符合");
             $("#wc9").removeClass("no").addClass("is");
@@ -140,11 +140,11 @@ window.onload = function(){
 // 吸气峰值压力
     // 10
     $("#xqfh13").blur(function(){
-        $("#xqfhp11").val((parseInt($("#xqfh11").val())+parseInt($("#xqfh12").val())+parseInt($("#xqfh13").val()))/3);
+        $("#xqfhp11").val(((parseFloat($("#xqfh11").val())+parseFloat($("#xqfh12").val())+parseFloat($("#xqfh13").val()))/3).toFixed(1));
     });
     $("#xqfc13").blur(function(){
-        $("#xqfcp12").val((parseInt($("#xqfc11").val())+parseInt($("#xqfc12").val())+parseInt($("#xqfc13").val()))/3);
-        $("#xqf1Swc").val(parseInt($("#xqfhp11").val())-parseInt($("#xqfcp12").val()));
+        $("#xqfcp12").val(((parseFloat($("#xqfc11").val())+parseFloat($("#xqfc12").val())+parseFloat($("#xqfc13").val()))/3).toFixed(1));
+        $("#xqf1Swc").val(parseFloat($("#xqfhp11").val())-parseFloat($("#xqfcp12").val()));
         if($("#xqf1Swc").val() >= 7 && $("#xqf1Swc").val() <= 13){
             $("#wc10").text("符合");
             $("#wc10").removeClass("no").addClass("is");
@@ -155,11 +155,11 @@ window.onload = function(){
     });
     // 20
     $("#xqfh23").blur(function(){
-        $("#xqfhp21").val((parseInt($("#xqfh21").val())+parseInt($("#xqfh22").val())+parseInt($("#xqfh23").val()))/3);
+        $("#xqfhp21").val(((parseFloat($("#xqfh21").val())+parseFloat($("#xqfh22").val())+parseFloat($("#xqfh23").val()))/3).toFixed(1));
     });
     $("#xqfc23").blur(function(){
-        $("#xqfcp22").val((parseInt($("#xqfc21").val())+parseInt($("#xqfc22").val())+parseInt($("#xqfc23").val()))/3);
-        $("#xqf2Swc").val(parseInt($("#xqfhp21").val())-parseInt($("#xqfcp22").val()));
+        $("#xqfcp22").val(((parseFloat($("#xqfc21").val())+parseFloat($("#xqfc22").val())+parseFloat($("#xqfc23").val()))/3).toFixed(1));
+        $("#xqf2Swc").val(parseFloat($("#xqfhp21").val())-parseFloat($("#xqfcp22").val()));
         if($("#xqf2Swc").val() >= 17 && $("#xqf2Swc").val() <= 23){
             $("#wc11").text("符合");
             $("#wc11").removeClass("no").addClass("is");
@@ -170,11 +170,11 @@ window.onload = function(){
     });
     // 30
     $("#xqfh33").blur(function(){
-        $("#xqfhp31").val((parseInt($("#xqfh31").val())+parseInt($("#xqfh32").val())+parseInt($("#xqfh33").val()))/3);
+        $("#xqfhp31").val(((parseFloat($("#xqfh31").val())+parseFloat($("#xqfh32").val())+parseFloat($("#xqfh33").val()))/3).toFixed(1));
     });
     $("#xqfc33").blur(function(){
-        $("#xqfcp32").val((parseInt($("#xqfc31").val())+parseInt($("#xqfc32").val())+parseInt($("#xqfc33").val()))/3);
-        $("#xqf3Swc").val(parseInt($("#xqfhp31").val())-parseInt($("#xqfcp32").val()));
+        $("#xqfcp32").val(((parseFloat($("#xqfc31").val())+parseFloat($("#xqfc32").val())+parseFloat($("#xqfc33").val()))/3).toFixed(1));
+        $("#xqf3Swc").val(parseFloat($("#xqfhp31").val())-parseFloat($("#xqfcp32").val()));
         if($("#xqf3Swc").val() >= 27 && $("#xqf3Swc").val() <= 33){
             $("#wc12").text("符合");
             $("#wc12").removeClass("no").addClass("is");
@@ -186,11 +186,11 @@ window.onload = function(){
 // 呼气末正压
     // 2
     $("#hqmh13").blur(function(){
-        $("#hqmhp11").val((parseInt($("#hqmh12").val())+parseInt($("#hqmh12").val())+parseInt($("#hqmh13").val()))/3);
+        $("#hqmhp11").val(((parseFloat($("#hqmh12").val())+parseFloat($("#hqmh12").val())+parseFloat($("#hqmh13").val()))/3).toFixed(1));
     });
     $("#hqmc13").blur(function(){
-        $("#hqmcp12").val((parseInt($("#hqmc11").val())+parseInt($("#hqmc12").val())+parseInt($("#hqmc13").val()))/3);
-        $("#hqm1Swc").val(parseInt($("#hqmhp11").val())-parseInt($("#hqmcp12").val()));
+        $("#hqmcp12").val(((parseFloat($("#hqmc11").val())+parseFloat($("#hqmc12").val())+parseFloat($("#hqmc13").val()))/3).toFixed(1));
+        $("#hqm1Swc").val(parseFloat($("#hqmhp11").val())-parseFloat($("#hqmcp12").val()));
         if($("#hqm1Swc").val() >= 0 && $("#hqm1Swc").val() <= 4){
             $("#wc13").text("符合");
             $("#wc13").removeClass("no").addClass("is");
@@ -201,11 +201,11 @@ window.onload = function(){
     });
     // 5
     $("#hqmh23").blur(function(){
-        $("#hqmhp21").val((parseInt($("#hqmh21").val())+parseInt($("#hqmh22").val())+parseInt($("#hqmh23").val()))/3);
+        $("#hqmhp21").val(((parseFloat($("#hqmh21").val())+parseFloat($("#hqmh22").val())+parseFloat($("#hqmh23").val()))/3).toFixed(1));
     });
     $("#hqmc23").blur(function(){
-        $("#hqmcp22").val((parseInt($("#hqmc21").val())+parseInt($("#hqmc22").val())+parseInt($("#hqmc23").val()))/3);
-        $("#hqm2Swc").val(parseInt($("#hqmhp21").val())-parseInt($("#hqmcp22").val()));
+        $("#hqmcp22").val(((parseFloat($("#hqmc21").val())+parseFloat($("#hqmc22").val())+parseFloat($("#hqmc23").val()))/3).toFixed(1));
+        $("#hqm2Swc").val(parseFloat($("#hqmhp21").val())-parseFloat($("#hqmcp22").val()));
         if($("#hqm2Swc").val() >= 3 && $("#hqm2Swc").val() <= 7){
             $("#wc14").text("符合");
             $("#wc14").removeClass("no").addClass("is");
@@ -216,11 +216,11 @@ window.onload = function(){
     });
     // 15
     $("#hqmh33").blur(function(){
-        $("#hqmhp31").val((parseInt($("#hqmh31").val())+parseInt($("#hqmh32").val())+parseInt($("#hqmh33").val()))/3);
+        $("#hqmhp31").val(((parseFloat($("#hqmh31").val())+parseFloat($("#hqmh32").val())+parseFloat($("#hqmh33").val()))/3).toFixed(1));
     });
     $("#hqmc33").blur(function(){
-        $("#hqmcp32").val((parseInt($("#hqmc31").val())+parseInt($("#hqmc32").val())+parseInt($("#hqmc33").val()))/3);
-        $("#hqm3Swc").val(parseInt($("#hqmhp31").val())-parseInt($("#hqmcp32").val()));
+        $("#hqmcp32").val(((parseFloat($("#hqmc31").val())+parseFloat($("#hqmc32").val())+parseFloat($("#hqmc33").val()))/3).toFixed(1));
+        $("#hqm3Swc").val(parseFloat($("#hqmhp31").val())-parseFloat($("#hqmcp32").val()));
         if($("#hqm3Swc").val() >= 13 && $("#hqm3Swc").val() <= 17){
             $("#wc15").text("符合");
             $("#wc15").removeClass("no").addClass("is");
